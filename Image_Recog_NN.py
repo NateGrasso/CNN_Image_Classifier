35abf3f9ca8e26145f32c7afe4a33c57d6cc31e1
Nathan Grasso <nathangrasso@yahoo.com>
2023-12-05 16:24:40 -0700

    Converted to OOP and fixed import issues

--- Image_Recog_NN.py
+++ Image_Recog_NN.py
@@ -1,158 +1,125 @@
-"""
-Name: Nathan Grasso
-Purpose: Create a CNN to classify images using tensorflow and keras
-Date: November 13, 2024
-"""
+# Name: Nathan Grasso
+# Date: 12/5/2023
+# Purpose: Create a CNN that can identify images of Cats and Dogs. Make the model Object Oriented for future use.
+
 import tensorflow as tf
 import keras 
-from keras.models import Sequential
-from keras.models import load_model
-from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
-from keras.metrics import Precision, Recall, BinaryAccuracy
+from keras import models, layers, metrics
 import os
 import cv2
 import imghdr
 import numpy as np
 from matplotlib import pyplot as plt
 
+class ImageClassifier:
+    def __init__(self, data_dir='data', image_exists=['jpeg', 'jpg', 'bmp', 'png']):
+        self.data_dir = data_dir
+        self.image_exists = image_exists
+        self.model = self.build_model()
 
-## Limit GPU memory consumption to avoid Out Of Memory errors
+    def limit_gpu_memory(self):
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
 
-#Grab the available GPU
-gpus = tf.config.experimental.list_physical_devices('GPU')
-#limit GPU so that tensorflow only uses what it needs
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+    def filter_unsupported_images(self):
+        for image_class in os.listdir(self.data_dir):
+            for image in os.listdir(os.path.join(self.data_dir, image_class)):
+                image_path = os.path.join(self.data_dir, image_class, image)
+                try:
+                    img = cv2.imread(image_path)
+                    tip = imghdr.what(image_path)
+                    if tip not in self.image_exists:
+                        print(f'Image not in the approved extension list {image_path}')
+                        os.remove(image_path)
+                except Exception as e:
+                    print(f'Issue with image {image_path}')
 
-# Create a variable that points to our image or data folder
-data_dir = 'data'
-# image loop to find acceptable image extensions 
-image_exists = ['jpeg', 'jpg', 'bmp', 'png']
+    def load_data(self):
+        data = tf.keras.utils.image_dataset_from_directory(self.data_dir)
+        return data
 
-##check whats in the data folder
-    # print(os.listdir(data_dir))
-##list out all images
-    # print(os.listdir(os.path.join(data_dir, "Dogs")))
+    def preprocess_data(self, data):
+        scale_data = data.map(lambda x, y: (x / 255, y))
+        return scale_data
 
-for image_class in os.listdir(data_dir):#looping through the folder for classes
-    for image in os.listdir(os.path.join(data_dir, image_class)): #joining looping through each image
-        image_path = os.path.join(data_dir, image_class, image)# 
-        try:
-            img = cv2.imread(image_path)#opens image as an numpy array
-            tip = imghdr.what(image_path)#checks the extension
-            if tip not in image_exists:
-                print('Image not in the approved extension list {}'.format(image_path))
-                os.remove(image_path)#if extension is not in list we remove it
-        except Exception as e:
-            print('Issue with image {}'.format(image_path))
+    def split_data(self, scale_data):
+        train_size = int(len(scale_data) * .76)
+        val_size = int(len(scale_data) * .2)
+        test_size = int(len(scale_data) * .1)
 
-##LOAD DATA
-data = tf.keras.utils.image_dataset_from_directory('data')#keras will create the classes and resize the images for me with this line
-data_iterator = data.as_numpy_iterator()# this allows access to the data we created in the above line of code so we can loop through it
-batch = data_iterator.next()# this is grabbing one batch of data
+        train = scale_data.take(train_size)
+        val = scale_data.skip(train_size).take(val_size)
+        test = scale_data.skip(train_size + val_size).take(test_size)  # Fixed the index error
 
-# visualize the classes and images so we know what class belongs to 0 and 1
-#fig, ax = plt.subplots(ncols=4, figsize=(20,20))#graph size
-#for index, img in enumerate(batch[0][:4]):#grab the first four images in the batch
-    #ax[index].imshow(img.astype(int))
-    #ax[index].title.set_text(batch[1][index])
-#plt.show() #explicit call for matplotlib to show the graph (0 = Cats, 1 = Dogs)
+        return train, val, test
 
-##PREPROCESSING DATA
-#Scale Data (we want the value of the images to be between 0-1 not 0-255)
-scale_data = data.map(lambda x,y: (x/255, y)) #x represents images, y represents labels
-scaled_iterator = data.as_numpy_iterator()
-batch = scaled_iterator.next()
+    def build_model(self):
+        model = models.Sequential()
+        model.add(keras.layers.Conv2D(16, (3, 3), 1, activation='relu', input_shape=(256, 256, 3)))
+        model.add(keras.layers.MaxPooling2D())
+        model.add(keras.layers.Conv2D(32, (3, 3), 1, activation='relu'))
+        model.add(keras.layers.MaxPooling2D())
+        model.add(keras.layers.Conv2D(16, (3, 3), 1, activation='relu'))
+        model.add(keras.layers.MaxPooling2D())
+        model.add(keras.layers.Flatten())
+        model.add(keras.layers.Dense(256, activation='relu'))
+        model.add(keras.layers.Dense(1, activation='sigmoid'))
 
-#checking if image works after being scaled
-#fig, ax = plt.subplots(ncols=4, figsize=(20,20))
-#for index, img in enumerate(batch[0][:4]):
-    #ax[index].imshow(img)# since we scaled to 0-1 we dont need it to be an int
-    #ax[index].title.set_text(batch[1][index])
-#plt.show()
+        model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
 
-#Split data into Training and Testing (allocating data)
+        return model
 
-#print(len(data))
+    def train_model(self, train_data, val_data, epochs=20):
+        logdir = 'logs'
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+        history = self.model.fit(train_data, epochs=epochs, validation_data=val_data, callbacks=[tensorboard_callback])
+        return history
 
-## PRE-PROCESSING DATA
+    def evaluate_model(self, test_data):
+        pre = tf.keras.metrics.Precision()
+        re = tf.keras.metrics.Recall()
+        acc = tf.keras.metrics.BinaryAccuracy()
 
-#allocating data to our different training and testing functions
-train_size = int(len(scale_data)* .76)
-val_size = int(len(scale_data)*.2)
-#test size is what we use after training to test model
-test_size = int(len(scale_data)*.1)
+        for batch in test_data.as_numpy_iterator():
+            x, y = batch
+            yhat = self.model.predict(x)
+            pre.update_state(y, yhat)
+            re.update_state(y, yhat)
+            acc.update_state(y, yhat)
 
-#telling the model how to use the data we divided and allocated
-train = scale_data.take(train_size)
-val = scale_data.skip(train_size).take(val_size)
-test = scale_data.skip(train_size-val_size).take(test_size)
+        precision = pre.result().numpy()
+        recall = re.result().numpy()
+        accuracy = acc.result().numpy()
 
-## BUILDING DEEP LEARNING MODEL
-model = Sequential()
-# add in the layers
-model.add(Conv2D(16, (3,3), 1, activation = 'relu', input_shape=(256,256,3)))
-model.add(MaxPooling2D())
+        return precision, recall, accuracy
 
-model.add(Conv2D(32,(3,3), 1, activation='relu'))
-model.add(MaxPooling2D())
+    def test_single_image(self, image_path):
+        img = cv2.imread(image_path)
+        resize = tf.image.resize(img, (256, 256))
+        plt.imshow(resize.numpy().astype(int))
+        plt.show()
 
-model.add(Conv2D(16, (3,3), 1, activation='relu'))
-model.add(MaxPooling2D())
+        np.expand_dims(resize, 0)
+        yhat = self.model.predict(np.expand_dims(resize / 255, 0))
 
-model.add(Flatten())
+        if yhat > 0.5:
+            print(f"This image is of a Dog. {yhat}")
+        else:
+            print(f"This image is of a Cat.{yhat}")
 
-model.add(Dense(256,activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+    def save_model(self, model_path='models/dogcatclassifier.h5'):
+        self.model.save(model_path)
 
-# Compile the model
-model.compile('adam', loss=tf.losses.BinaryCrossentropy(),metrics=['accuracy'])
-#model.summary()
-
-#TRAIN THE MODEL
-
-#log training 
-logdir='logs'
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
-history = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
-#print(history.history)
-
-#plot the performance of training
-#fig = plt.figure()
-#plt.plot(history.history['loss'], color = 'red', label='loss')
-#plt.plot(history.history['val_loss'], color='blue', label = 'val_loss')
-#fig.suptitle('Loss', fontsize=20)
-#plt.legend(loc="upper left")
-#plt.show()
-
-#Evaluate results
-pre = Precision()
-re = Recall()
-acc = BinaryAccuracy()
-
-#loop through the batches in the data set aside for test
-for batch in test.as_numpy_iterator():
-    x, y = batch
-    yhat = model.predict(x)
-    pre.update_state(y, yhat)
-    re.update_state(y, yhat)
-    acc.update_state(y, yhat)
-print(f"Precision:{pre.result().numpy()}, Recall:{re.result().numpy()}, Accuracy:{acc.result().numpy()}")
-
-#test the image classifier with images it has never seen before
-img = cv2.imread('Dogtest.jpg')
-resize = tf.image.resize(img, (256,256))
-plt.imshow(resize.numpy().astype(int))
-plt.show()
-
-#turn single image into the correct shape, the model expects a batch so this will help
-np.expand_dims(resize,0)
-yhat = model.predict(np.expand_dims(resize/255, 0))
-
-if yhat > 0.5:
-    print("This image is of a Dog.")
-else:
-    print("This image is of a Cat.")
-
-## SAVE THE MODEL
-#model.save(os.path.join('models', 'dogcatclassifier.h5'))
+# run model on individual image:
+classifier = ImageClassifier()
+classifier.limit_gpu_memory()
+classifier.filter_unsupported_images()
+data = classifier.load_data()
+scaled_data = classifier.preprocess_data(data)
+train_data, val_data, test_data = classifier.split_data(scaled_data)
+history = classifier.train_model(train_data, val_data)
+precision, recall, accuracy = classifier.evaluate_model(test_data)
+print(f"Precision: {precision}, Recall: {recall}, Accuracy: {accuracy}")
+classifier.test_single_image("Cattest2.jpeg")
+#classifier.save_model()
