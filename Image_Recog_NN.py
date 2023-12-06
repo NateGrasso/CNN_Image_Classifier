03b6500aa23a38ece8d2bb4492a374c9c9ff1fdf
Nathan Grasso <nathangrasso@yahoo.com>
2023-12-05 17:09:38 -0700

    added comments to help understand new OOP structure

--- Image_Recog_NN.py
+++ Image_Recog_NN.py
@@ -18,44 +18,52 @@ class ImageClassifier:
         self.model = self.build_model()
 
     def limit_gpu_memory(self):
+        #limit GPU memory to avoid OOM errors
         gpus = tf.config.experimental.list_physical_devices('GPU')
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
 
     def filter_unsupported_images(self):
+        #loop through folder for classes
         for image_class in os.listdir(self.data_dir):
             for image in os.listdir(os.path.join(self.data_dir, image_class)):
                 image_path = os.path.join(self.data_dir, image_class, image)
                 try:
-                    img = cv2.imread(image_path)
-                    tip = imghdr.what(image_path)
+                    img = cv2.imread(image_path)#opens image as numpy array
+                    tip = imghdr.what(image_path)#checks extension
                     if tip not in self.image_exists:
                         print(f'Image not in the approved extension list {image_path}')
-                        os.remove(image_path)
+                        os.remove(image_path)#remove image if not in list
                 except Exception as e:
                     print(f'Issue with image {image_path}')
 
     def load_data(self):
+        #keras creates classes and will resize the image for me
         data = tf.keras.utils.image_dataset_from_directory(self.data_dir)
         return data
 
     def preprocess_data(self, data):
+        #we want our data to be between 0-1 not 0-255
+        #x represents images and y represents labels 
         scale_data = data.map(lambda x, y: (x / 255, y))
         return scale_data
 
     def split_data(self, scale_data):
+        #We will split our data into train, validation, and test (the majority will be in train)
         train_size = int(len(scale_data) * .76)
         val_size = int(len(scale_data) * .2)
         test_size = int(len(scale_data) * .1)
-
+        
+        #telling the model how to use our data
         train = scale_data.take(train_size)
         val = scale_data.skip(train_size).take(val_size)
-        test = scale_data.skip(train_size + val_size).take(test_size)  # Fixed the index error
+        test = scale_data.skip(train_size + val_size).take(test_size)
 
         return train, val, test
 
     def build_model(self):
         model = models.Sequential()
+        # adding the layers of the model
         model.add(keras.layers.Conv2D(16, (3, 3), 1, activation='relu', input_shape=(256, 256, 3)))
         model.add(keras.layers.MaxPooling2D())
         model.add(keras.layers.Conv2D(32, (3, 3), 1, activation='relu'))
@@ -71,6 +79,7 @@ class ImageClassifier:
         return model
 
     def train_model(self, train_data, val_data, epochs=20):
+        #log our training epochs
         logdir = 'logs'
         tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
         history = self.model.fit(train_data, epochs=epochs, validation_data=val_data, callbacks=[tensorboard_callback])
@@ -100,6 +109,7 @@ class ImageClassifier:
         plt.imshow(resize.numpy().astype(int))
         plt.show()
 
+        #turn a single image into the correct shape
         np.expand_dims(resize, 0)
         yhat = self.model.predict(np.expand_dims(resize / 255, 0))
 
